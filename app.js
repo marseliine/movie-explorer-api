16ac97ef7c72a064a08d1d109f7eaa7044726d2c
marseliine <78792953+marseliine@users.noreply.github.com>
2023-10-30 18:33:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ const corsOptions = {
   origin: allowedCors,
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: true,
   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 };
 
