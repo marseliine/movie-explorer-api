951710092cbc63e7e7aede2ffeb260cbe2cf2ac5
marseliine <78792953+marseliine@users.noreply.github.com>
2023-11-01 01:12:44 +0300

    update

--- routes/logout.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { logout } = require("../controllers/logout");
-
-router.get("/logout", logout);
-
-module.exports = router;
--- routes/register.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { createUser } = require("../controllers/register");
-
-const { createUserValidator } = require("../validation/validationRules");
-
-router.post("/signup", createUserValidator, createUser);
-
-module.exports = router;
